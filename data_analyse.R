1f5e243fcf2317b0b549ec63b0a667a026099f29
JeyaniGC <72611556+JeyaniGC@users.noreply.github.com>
2022-09-29 23:21:55 +0200

    Add files via upload
    load code data analyse

---
+++ data_analyse.R
@@ -0,0 +1,40 @@
+rm(list=ls()) 
+
+library(readr)
+library(caret)
+library(rpart.plot)
+library(pROC)
+library(knitr)
+library(corrplot)
+library(FactoMineR)
+library(tidyr)
+
+train_features <- read_csv("data/train_features.csv")
+summary(train_features)
+spec(train_features)
+
+# Sélectionn des variables 
+# matrice de corrélation
+train_ft <- train_features %>% 
+  dplyr::select(-sig_id, -cp_type, -cp_dose, -cp_time)
+
+train_cor <- cor(train_ft)
+
+# plot qui ne marche point
+corrplot(train_cor, tl.cex = 0.5)
+
+var <- findCorrelation(train_cor, cutoff = 0.95, names = T)
+# pas de VA corrélés
+
+train_colin <- caret::findLinearCombos(train_ft)
+train_colin$remove
+# pas de colin 
+
+# clean dataset
+
+# réduction des dimension et ACP
+# visuslisation
+train_pca <- PCA(train_ft)
+
+
+          
