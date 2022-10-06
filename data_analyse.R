eaa1bae969fb1de05eb73965c0069fa7720cd0d8
CNOV0 <mayazygadlo@DESKTOP-TARTU7S.localdomain>
2022-10-06 21:38:02 +0200

    Ajout d'un plot sur cp_X.

--- data_analyse.R
+++ data_analyse.R
@@ -49,14 +49,10 @@ train_colin$remove
 ################################################################################
 #                                   plot                                       #
 ################################################################################
-p1 <- train_features %>%
-    count(cp_type) %>%
-    add_tally(n, name="total") %>%
-    mutate(perc = n/total) %>%
-    ggplot(aes(cp_type, perc, fill = cp_type)) +
-    geom_col() + 
-    scale_y_continuous()
-p1
+
+barplot(summary(as.factor(train_features$cp_type)))
+barplot(summary(as.factor(train_features$cp_time)))
+barplot(summary(as.factor(train_features$cp_dose)))
 
 # reduction des dimension et ACP
 # visualisation
